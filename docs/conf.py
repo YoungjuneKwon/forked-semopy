ee0fbd98cae37cc34abed9d032ee68d4417a1e96
Georgy Mesheryakov <Herrberg@yandex.ru>
2019-05-22 17:59:49 +0300

    Fix conf #4

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 
-autodoc_mock_imports = ['portmin']
+autodoc_mock_imports = ['portmin', 'graphviz']
 
 
 # -- Project information -----------------------------------------------------
